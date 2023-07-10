d0f4b438c7f88c916c1c613e93e2477307065c3d
MANISHA KUMARI <87902901+Technocharm@users.noreply.github.com>
2023-07-11 00:35:29 +0530

    Update teapot.c

--- teapot.c
+++ teapot.c
@@ -1,57 +1,59 @@
-#include <GL/glut.h>
 #include <stdio.h>
-float light_pos[]={2,2,2,1};
-float ambient[]={1,1,1,1};
+#include <GL/glut.h>
+
+float light_pos[] = {27,80,2,3};
+float ambient[] = {1,1,1,1};
 
 void align(){
-	glRotatef(50,0,1,0);
-	glRotatef(10,-1,0,0);
-	glRotatef(10,0,0,-1);
+    glRotatef(50,0,1,0);
+    glRotatef(10,-1,0,0);
+    glRotatef(11.7,0,0,-1);
 }
 
-void obj(float tx,float ty, float tz, float sx, float sy, float sz){
-	align();
-	glTranslatef(tx,ty,tz);
-	glScalef(sx,sy,sz);
-	glutSolidCube(1);
-	glLoadIdentity();
+void obj(float tx,float ty,float tz,float sx, float sy, float sz){
+    align();
+    glTranslatef(tx,ty,tz);
+    glScalef(sx,sy,sz);
+    glutSolidCube(1);
+    glLoadIdentity();
 }
 
 void display(){
-	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-	//wall
-	obj(0,0,0.5,1,1,0.04);
-	obj(-0.5,0,0,0.04,1,1);
-	obj(0,-0.5,0,1,0.04,1);
+    glViewport(0,0,700,700);
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    obj(0,0,0.5,1,1,0.04);
+    obj(0,-0.5,0,1,0.04,1);
+    obj(-0.5,0,0,0.04,1,1);
 
-	//leg
-	obj(-0.2,-0.5,0.2,0.02,0.2,0.02);
-    obj(0.5,-0.3,0,0.02,0.2,0.02);
-    obj(0,-0.3,-0.5,0.02,0.2,0.02);
-    obj(0.5,-0.3,-0.5,0.02,0.2,0.02);
+    obj(0,-0.3,0,0.02,0.2,0.02);
+    obj(0,-0.3,-0.4,0.02,0.2,0.02);
+    obj(0.4,-0.3,0,0.02,0.2,0.02);
+    obj(0.4,-0.3,-0.4,0.02,0.2,0.02);
 
-    //top
-	obj(0,-0.228,0,0.9,0.04,0.9);
+    obj(0.2,-0.18,-0.2,0.6,0.02,0.6);
 
-	glPushMatrix();
-    glTranslatef(-0.02,-0.1,0.02);
+    glPushMatrix();
+    align();
+    glTranslatef(0.3,-0.1,-0.3);
     glutSolidTeapot(0.10);
     glPopMatrix();
-	glFlush();
+
+    glFlush();
+    glLoadIdentity();
 }
 
-int main(int argc,char ** argv){
-	glutInit(&argc,argv);
-	glutInitDisplayMode(GLUT_RGB|GLUT_SINGLE|GLUT_DEPTH);
-	glutCreateWindow("1CR20CS109 Teapot");
-	glutInitWindowPosition(0,0);
-	glutInitWindowSize(500,500);
-	glutDisplayFunc(display);
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
-	glLightfv(GL_LIGHT0,GL_POSITION,light_pos);
-	glMaterialfv(GL_FRONT,GL_AMBIENT,ambient);
-	glEnable(GL_DEPTH_TEST);
-	glutMainLoop();
-	return 0;
+int main(int argc, char ** argv){
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_RGB|GLUT_DEPTH|GLUT_SINGLE);
+    glutInitWindowPosition(0,0);
+    glutInitWindowSize(700,700);
+    glutCreateWindow("Teapot");
+    glutDisplayFunc(display);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glMaterialfv(GL_FRONT,GL_AMBIENT,ambient);
+    glLightfv(GL_LIGHT0,GL_POSITION,light_pos);
+    glEnable(GL_DEPTH_TEST);
+    glutMainLoop();
+    return 0;
 }
